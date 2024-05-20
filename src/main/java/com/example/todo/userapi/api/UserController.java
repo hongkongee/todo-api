c8d52ebfc5fd56cbc43ddb45067b18dcd8fcac24
jeongwon <tjtkdvl@gmail.com>
2024-05-20 15:55:15 +0900

    #4. 회원 가입 처리

--- src/main/java/com/example/todo/userapi/api/UserController.java
+++ src/main/java/com/example/todo/userapi/api/UserController.java
@@ -1,13 +1,15 @@
 package com.example.todo.userapi.api;
 
+import com.example.todo.userapi.dto.request.UserSignInRequestDTO;
+import com.example.todo.userapi.dto.request.UserSignUpRequestDTO;
+import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
 import com.example.todo.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -34,4 +36,58 @@ public class UserController {
 
         return ResponseEntity.ok().body(resultFlag);
     }
+
+    // 회원 가입 요청 처리
+    // POST: /api/auth
+    @PostMapping
+    public ResponseEntity<?> signUp(
+            @Validated @RequestBody UserSignUpRequestDTO dto,
+            BindingResult result
+    ) {
+        log.info("/api/auth POST - {}", dto);
+
+        if (result.hasErrors()) {
+            log.warn(result.toString());
+            return ResponseEntity.badRequest()
+                    .body(result.getFieldErrors());
+        }
+
+        try {
+            UserSignUpResponseDTO responseDTO = userService.create(dto);
+            return ResponseEntity.ok().body(responseDTO);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+    // 로그인 요청 처리 메서드를 선언하세요. POST: /api/auth/signin
+    // LoginRequestDTO 클래스를 생성해서 요청 값을 받아주세요.
+    // 서비스로 넘겨서, 로그인 유효성을 검증하세요 (비밀번호 암호화 되어 있어요)
+    // 로그인 결과를 응답 상태 코드로 구분해서 보내 주세요.
+    // 로그인이 성공했다면 200, 실패라면 400을 보내주세요 (에러 메세지를 상황에 따라 다르게 전달해 주세요)
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(
+            @Validated @RequestBody UserSignInRequestDTO dto,
+            BindingResult result
+    ) {
+        log.info("/api/auth/signin: POST! - {}", dto);
+
+
+        try {
+            boolean flag = userService.checkLogin(dto);
+            if (flag) {
+                return ResponseEntity.ok().body("로그인 성공!");
+            } else {
+                return ResponseEntity.badRequest().body("비밀번호가 맞지 않습니다.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("이메일이 존재하지 않습니다.");
+        }
+
+
+    }
+
 }
--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -1,13 +1,15 @@
 package com.example.todo.userapi.service;
 
+import com.example.todo.userapi.dto.request.UserSignInRequestDTO;
+import com.example.todo.userapi.dto.request.UserSignUpRequestDTO;
+import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
 import com.example.todo.userapi.entity.User;
 import com.example.todo.userapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Slf4j
@@ -15,11 +17,47 @@ import java.util.List;
 @Transactional
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean isDuplicate(String email) {
         if (userRepository.existsByEmail(email)) {
             log.warn("이메일이 중복되었습니다. - {}", email);
             return true;
         } else return false;
+    }
+
+    public UserSignUpResponseDTO create(final UserSignUpRequestDTO dto) throws Exception {
+        String email = dto.getEmail();
+
+        if (isDuplicate(email)) {
+            throw new RuntimeException("중복된 이메일 입니다.");
+        }
+
+        // 패스워드 인코딩
+        String encoded = passwordEncoder.encode(dto.getPassword());
+        dto.setPassword(encoded);
+
+        // dto를 User Entity로 변환해서 저장
+        User saved = userRepository.save(dto.toEntity());
+        log.info("회원 가입 정상 수행됨! - saved user - {}", saved);
+
+        return new UserSignUpResponseDTO(saved);
+
+
+    }
+
+    public boolean checkLogin(UserSignInRequestDTO dto) throws Exception {
+        String encoded = passwordEncoder.encode(dto.getPassword());
+
+        User loginUser = null;
+        try {
+            loginUser = userRepository.findByEmail(dto.getEmail());
+        } catch (NullPointerException e) {
+            throw new RuntimeException("존재하지 않는 이메일 입니다.");
+        }
+
+
+        return loginUser.getPassword() == encoded; // 같으면 로그인 성공
+        
     }
 }
